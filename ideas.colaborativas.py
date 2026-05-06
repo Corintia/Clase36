861929f6c3ed61a9e3b29947540cc640fb309f6b
Corintia Urrutia <94489130+Corintia@users.noreply.github.com>
2026-05-06 15:45:25 -0600

    Update ideas.colaborativas.py
    hicimos la lista  tipo  diccionario

--- ideas.colaborativas.py
+++ ideas.colaborativas.py
@@ -5,6 +5,10 @@
 # Ejemplo: {"nombre": "Profesor/a", "idea": "Un asistente de IA para organizar libros en la biblioteca"}
 
 ideas_de_proyectos_ia = [
+    {"nombre":"alex","idea":"una ia que cree recetas de comida"}
+    {"nombre":"alex","idea":"una ia que haga papiroflexia"}
+    {"nombre":"alex","idea":"una ia que detecte bichos"}
+    {"nombre":"alex","idea":"una ia que haga arte"}
     # --- ¡Añade tu propia contribución debajo de esta línea! ---
     # No borres lo que otros compañeros hayan añadido.
     # Asegúrate de que cada idea sea un diccionario y esté separada por una coma si no es la última.
